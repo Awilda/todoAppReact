cb70b777765a0443e1bc9213b5b421dd0845bf55
Awilda <awildacantres@gmail.com>
2017-07-21 11:05:16 -0400

    add directories

---
+++ public/app/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+
+	render(){
+		return (
+			<div>
+				First Run!
+			</div>
+		)
+	}
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
